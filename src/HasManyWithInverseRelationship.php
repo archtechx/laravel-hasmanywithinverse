dc3a42c56c25765625d8cfb5623ecc3a0a355092
Viktor Szépe <viktor@szepe.net>
2020-04-02 04:41:40 +0200

    toHasManyWithInverseRelationship::create PHPDoc

--- src/HasManyWithInverseRelationship.php
+++ src/HasManyWithInverseRelationship.php
@@ -8,15 +8,23 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class HasManyWithInverseRelationship extends HasMany
 {
-    protected string $relationToParent;
+    /**
+     * @var string
+     */
+    protected $relationToParent;
 
-    public function __construct(Builder $query, Model $parent, $foreignKey, $localKey, $relationToParent)
+    public function __construct(Builder $query, Model $parent, string $foreignKey, string $localKey, string $relationToParent)
     {
         $this->relationToParent = $relationToParent;
 
         parent::__construct($query, $parent, $foreignKey, $localKey);
     }
 
+    /**
+     * @param array<mixed, mixed> $attributes
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
     public function create(array $attributes = [])
     {
         return tap($this->related->newInstance($attributes), function ($instance) {
